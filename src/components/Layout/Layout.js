7deb1f3b22724681d14b0aafd05fd6f50a44fc3f
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-01-24 03:52:52 +0200

    add burger folders

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
+import Classes from './Layout.module.css'
 const Layout = (props) => (
     <Aux>
         <div>tool bar ....</div>
-        <main>
+        <main className={Classes.Content}>
             {props.children}
         </main>
     </Aux>
 );
+
+export default Layout;
