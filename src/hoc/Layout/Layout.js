db3c0fcf374e933f2cf081e9b032b8be7c399ff5
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-03-21 04:19:13 +0200

    convet Layout to be a functional component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
-import Aux from '../../hoc/Aux/Aux';
-import Classes from './Layout.module.css';
-import ToolBar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
-import {connect} from 'react-redux';
+import React, { useState } from "react";
+import Aux from "../../hoc/Aux/Aux";
+import Classes from "./Layout.module.css";
+import ToolBar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import { connect } from "react-redux";
 
-class Layout extends Component{
-    
-    state={
-        showSideDrawer:false
-    }
-    sideDrawerClosedHandler=()=>{
-        this.setState({showSideDrawer: false});
-    }
+const Layout = props => {
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+  const sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerOpenHandler =()=>{
-        this.setState({showSideDrawer: true});
-    }
-    render(){
-        return (
-            <Aux>
-                <ToolBar isAuthenticated = {this.props.isAuthenticated} menuButtonClicked={this.sideDrawerOpenHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
-                <main className={Classes.Content}>
-                    {this.props.children}
-                </main>
-            </Aux>
-       );
-    }
-}
+  const sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true });
+  };
+  return (
+    <Aux>
+      <ToolBar
+        isAuthenticated={props.isAuthenticated}
+        menuButtonClicked={sideDrawerOpenHandler}
+      />
+      <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler} />
+      <main className={Classes.Content}>{props.children}</main>
+    </Aux>
+  );
+};
 
-const MapStateToProps = (state) => {
-    return {
-        isAuthenticated: state.auth.token !== null
-    };
-}
+const MapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 export default connect(MapStateToProps)(Layout);
