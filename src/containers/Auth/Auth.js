289846bc59dd8e883f54786e77484becb881ae60
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-03-21 05:39:22 +0200

    make Auth a functional component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,115 +1,118 @@
-import React, { Component } from 'react';
-import Input from '../../components/UI/Input/Input';
-import Button from '../../components/UI/Button/Button';
-import Spinner from '../../components/UI/Spinner/Spinner';
-import Classes from './Auth.module.css';
-import { connect } from 'react-redux';
-import * as actionCreators from '../../store/actions/index';
-import { Redirect } from 'react-router';
+import React, { Component, useState, useEffect } from "react";
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import Classes from "./Auth.module.css";
+import { connect } from "react-redux";
+import * as actionCreators from "../../store/actions/index";
+import { Redirect } from "react-router";
 
-class Auth extends Component {
-    state = {
-        singUp: true,
-        authForm: {
-            email: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'email',
-                    placeholder: 'email'
-                },
-                value: ''
-            },
-            password: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'password',
-                    placeholder: 'password'
-                },
-                value: ''
-            }
-        }
-    };
-
-    inputChangedHandler = (event, id) => {
-        event.preventDefault();
-        let updatedForm = { ...this.state.authForm };
-        let updatedFormElement = { ...updatedForm[id] };
-        updatedFormElement.value = event.target.value;
-        updatedForm[id] = updatedFormElement;
-        this.setState({ authForm: updatedForm });
+const Auth = props => {
+  const [singUp, setSignUp] = useState(true);
+  const [authForm, setAuthForm] = useState({
+    email: {
+      elementType: "input",
+      elementConfig: {
+        type: "email",
+        placeholder: "email"
+      },
+      value: ""
+    },
+    password: {
+      elementType: "input",
+      elementConfig: {
+        type: "password",
+        placeholder: "password"
+      },
+      value: ""
     }
+  });
 
-    submintHandler = (event) => {
-        event.preventDefault();
-        const email = this.state.authForm.email.value;
-        const password = this.state.authForm.password.value;
+  const inputChangedHandler = (event, id) => {
+    event.preventDefault();
+    let updatedForm = { ...authForm };
+    let updatedFormElement = { ...updatedForm[id] };
+    updatedFormElement.value = event.target.value;
+    updatedForm[id] = updatedFormElement;
+    setAuthForm(updatedForm);
+  };
 
-        if (this.state.singUp) {
-            this.props.onSignUp({ email: email, password: password });
-        } else {
-            this.props.onSignIn({ email: email, password: password });
-        }
+  const submintHandler = event => {
+    event.preventDefault();
+    const email = authForm.email.value;
+    const password = authForm.password.value;
+
+    if (singUp) {
+      props.onSignUp({ email: email, password: password });
+    } else {
+      props.onSignIn({ email: email, password: password });
     }
+  };
 
-
-    getForm() {
-        let inputElements = [];
-        for (let key in this.state.authForm) {
-            inputElements.push({
-                id: key,
-                config: this.state.authForm[key]
-            });
-        }
-        inputElements = inputElements.map((element) => {
-            return <Input changed={(event) => this.inputChangedHandler(event, element.id)} elementType={element.config.elementType}
-                elementConfig={element.config.elementConfig}
-                value={element.config.value} key={element.id} />
-        });
-        let form = (
-            <form >
-                {inputElements}
-                <Button buttonType='Success' clicked={this.submintHandler}>Submit</Button>
-            </form>
-        );
-
-        if (this.props.loading) {
-            form = <Spinner />;
-        }
-        if(this.props.isAuthenticated){
-            form = <Redirect to={this.props.isBuilding? '/checkout': '/'}/>;
-        }
-        return form;
+  function getForm() {
+    let inputElements = [];
+    for (let key in authForm) {
+      inputElements.push({
+        id: key,
+        config: authForm[key]
+      });
     }
+    inputElements = inputElements.map(element => {
+      return (
+        <Input
+          changed={event => inputChangedHandler(event, element.id)}
+          elementType={element.config.elementType}
+          elementConfig={element.config.elementConfig}
+          value={element.config.value}
+          key={element.id}
+        />
+      );
+    });
+    let form = (
+      <form>
+        {inputElements}
+        <Button buttonType="Success" clicked={submintHandler}>
+          Submit
+        </Button>
+      </form>
+    );
 
-    switchHandler = () => {
-        this.setState({ singUp: !this.state.singUp });
+    if (props.loading) {
+      form = <Spinner />;
     }
-
-    render() {
-        return (
-            <div className={Classes.Auth}>
-                {this.getForm()}
-                <Button buttonType='Danger' clicked={this.switchHandler}>
-                    {this.state.singUp ? 'Siwtch to Login' : 'Switch To Singn Up'}
-                </Button>
-            </div>
-        );
+    if (props.isAuthenticated) {
+      form = <Redirect to={props.isBuilding ? "/checkout" : "/"} />;
     }
-}
+    return form;
+  }
 
-const mapStateToProps = (state) => {
-    return {
-        loading: state.auth.loading,
-        isAuthenticated: state.auth.token !== null,
-        isBuilding: state.burgerBuilder.building,
-    };
-}
+  const switchHandler = () => {
+    setSignUp(!singUp);
+  };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onSignUp: data => dispatch(actionCreators.authSignUp(data)),
-        onSignIn: data => dispatch(actionCreators.authSignIn(data)),
-    };
-}
+  return (
+    <div className={Classes.Auth}>
+      {getForm()}
+      <Button buttonType="Danger" clicked={switchHandler}>
+        {singUp ? "Siwtch to Login" : "Switch To Singn Up"}
+      </Button>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    isAuthenticated: state.auth.token !== null,
+    isBuilding: state.burgerBuilder.building
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSignUp: data => dispatch(actionCreators.authSignUp(data)),
+    onSignIn: data => dispatch(actionCreators.authSignIn(data))
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Auth);
