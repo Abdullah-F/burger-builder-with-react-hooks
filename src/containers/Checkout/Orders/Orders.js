49cc7e3853f028830a1236240443a0165f437e5e
Abdullah Fadhel <abdullahfadhel@outlook.com>
2020-03-21 04:49:01 +0200

    convert orders container to a functional component

--- src/containers/Checkout/Orders/Orders.js
+++ src/containers/Checkout/Orders/Orders.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
-import Order from '../../../components/Order/Order';
-import Axios from '../../../axios-orders';
-import WithErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
-import * as actionCreators from '../../../store/actions/index';
-import { connect } from 'react-redux';
+import React, { Component, useEffect } from "react";
+import Order from "../../../components/Order/Order";
+import Axios from "../../../axios-orders";
+import WithErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
+import * as actionCreators from "../../../store/actions/index";
+import { connect } from "react-redux";
 
-class Orders extends Component {
-    componentDidMount() {
-        this.props.onOrdersFetch(this.props.token);
-    }
+const Orders = props => {
+  useEffect(() => {
+    props.onOrdersFetch(this.props.token);
+  });
 
-    getOrders() {
-        return (this.props.orders.map((order) => {
-            return <Order key={order.id}
-                ingredients={order.ingredients}
-                price={order.price} />
-        }));
-    }
-    
-    render() {
-        return (
-            <div>
-                {this.getOrders()}
-            </div>
-        )
-    }
-}
+  function getOrders() {
+    return props.orders.map(order => {
+      return (
+        <Order
+          key={order.id}
+          ingredients={order.ingredients}
+          price={order.price}
+        />
+      );
+    });
+  }
 
-const mapStateToProps = (state)=>{
-    return {
-        orders: state.order.orders,
-        token: state.auth.token,
-    }
-}
-const mapDispatchToProps = (dispatch)=>{
-    return {
-        onOrdersFetch: (token) => dispatch(actionCreators.fetchOrders(token))
-    }
-}
+  return <div>{getOrders()}</div>;
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(WithErrorHandler(Orders, Axios));
+const mapStateToProps = state => {
+  return {
+    orders: state.order.orders,
+    token: state.auth.token
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    onOrdersFetch: token => dispatch(actionCreators.fetchOrders(token))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WithErrorHandler(Orders, Axios));
